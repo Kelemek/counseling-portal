83e136eb0c79f9ac708f7d7ec4512d8d3894f07a
Kelemek <markdlarson@me.com>
2025-10-29 07:30:28 -0500

    Remove forgot password link (not yet implemented)

--- app/src/app/login/page.tsx
+++ app/src/app/login/page.tsx
@@ -92,14 +92,6 @@ export default function LoginPage() {
               />
             </div>
 
-            <div className="flex items-center justify-between">
-              <div className="text-sm">
-                <Link href="/auth/forgot-password" className="font-medium text-indigo-600 hover:text-indigo-500">
-                  Forgot your password?
-                </Link>
-              </div>
-            </div>
-
             <button
               type="submit"
               disabled={loading}
